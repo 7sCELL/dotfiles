6a627b3a3477fe0d73fb7ca0540b2cc555553530
7sCELL <72708203+7sCELL@users.noreply.github.com>
2025-03-03 07:02:44 +0000

    Update install.sh
    added comments

--- install.sh
+++ install.sh
@@ -1,20 +1,36 @@
+# Install essential packages using pacman
 sudo pacman -S git terminator trash-cli curl kitty playerctl neovim polybar steam bluez bluez-utils blueman pulseaudio pulseaudio-bluetooth pavucontrol picom brightnessctl rofi gvfs-smb feh thunar grub-customizer bpytop ranger os-prober xorg-xrandr
+
+# Install additional packages using yay
 sudo yay -S kew
+
+# Start and enable the Bluetooth service
 sudo systemctl start bluetooth.service
 sudo systemctl enable bluetooth.service
-sudo cd /etc
-echo "GTK_THEME=Adwaita-dark" > environment
+
+# Change directory to /etc and set GTK theme to Adwaita-dark
+cd /etc
+echo "GTK_THEME=Adwaita-dark" | sudo tee environment
+
+# Return to the home directory
 cd
+
+# Open the picom configuration file with nvim (requires user interaction)
 sudo nvim /etc/xdg/picom.conf
+
+# Clone NvChad starter configuration for Neovim and open Neovim (requires user interaction)
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
-cd
-sudo cd /etc/X11/xorg.conf.d/
-echo "Section "InputClass"
+
+# Change directory to /etc/X11/xorg.conf.d/ and create a touchpad configuration file
+cd /etc/X11/xorg.conf.d/
+echo 'Section "InputClass"
         Identifier "libinput touchpad catchall"
         MatchIsTouchpad "on"
         MatchDevicePath "/dev/input/event*"
         Driver "libinput"
         Option "Tapping" "on"
         Option "NaturalScrolling" "True"
-EndSection" > touchpad-tap.conf
-echo "Script has finished successfully." 
+EndSection' | sudo tee touchpad-tap.conf
+
+# Indicate that the script has finished
+echo "Script has finished successfully."
